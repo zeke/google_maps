79cfd9d55999db5ed7a8a7daa55907cfa33beb2c
Russ Sherman <bhedana@gmail.com>
2009-07-07 12:25:38 -0500

    pass map to icon

--- lib/google_map/small_icon.rb
+++ lib/google_map/small_icon.rb
@@ -5,7 +5,7 @@ module GoogleMap
 
     alias_method :parent_initialize, :initialize
 
-    def initialize(color = 'red')
+    def initialize( map, color = 'red')
       parent_initialize(:width => 12,
                         :height => 20,
                         :shadow_width => 22,
@@ -15,7 +15,8 @@ module GoogleMap
                         :anchor_x => 6,
                         :anchor_y => 20,
                         :info_anchor_x => 5,
-                        :info_anchor_y => 1)
+                        :info_anchor_y => 1,
+                        :map => map)
     end
   end
 
